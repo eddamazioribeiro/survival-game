baae92e2dfb40a54d952c91051a593db70654023
Eduardo Ribeiro <eribeiro@pmi-ext.com>
2025-08-07 16:29:05 -0300

    Code refactoring: creating Player class

--- js/MainScene.js
+++ js/MainScene.js
@@ -1,3 +1,5 @@
+import Player from './Player.js';
+
 export default class MainScene extends Phaser.Scene {
 	constructor() {
 		super('MainScene');
@@ -5,16 +7,19 @@ export default class MainScene extends Phaser.Scene {
 
 	preload() {
 		console.log('preload');
-		this.load.atlas('townsfolk', 'assets/images/townsfolk.png', 'assets/images/townsfolk_atlas.json');
-		this.load.animation('townsfolk_anim', 'assets/images/townsfolk_anim.json');
+		Player.preload(this);
 	}
 
 	create() {
 		console.log('create');
-		this.player = new Phaser.Physics.Matter.Sprite(this.matter.world, 0, 0, 'townsfolk', 'townsfolk_m_idle_1')
-		this.add.existing(this.player);
-
-		this.inputKeys = this.input.keyboard.addKeys({
+		this.player = new Player({
+			scene: this,
+			x: 0,
+			y: 0, 
+			texture: 'townsfolk',
+			frame: 'townsfolk_m_idle_1'
+		});
+		this.player.inputKeys = this.input.keyboard.addKeys({
 			up: Phaser.Input.Keyboard.KeyCodes.W,
 			down: Phaser.Input.Keyboard.KeyCodes.S,
 			left: Phaser.Input.Keyboard.KeyCodes.A,
@@ -23,24 +28,6 @@ export default class MainScene extends Phaser.Scene {
 	}
 
 	update() {
-		this.player.anims.play('townsfolk_idle', true);
-		const speed = 2.5;
-		let playerVelocity = new Phaser.Math.Vector2();
-
-		if (this.inputKeys.left.isDown) {
-			playerVelocity.x = -1;
-		} else if (this.inputKeys.right.isDown) {
-			playerVelocity.x = 1;
-		}
-
-		if (this.inputKeys.up.isDown) {
-			playerVelocity.y = -1;
-		} else if (this.inputKeys.down.isDown) {
-			playerVelocity.y = 1;
-		}
-
-		playerVelocity.normalize();
-		playerVelocity.scale(speed);
-		this.player.setVelocity(playerVelocity.x, playerVelocity.y);
+		this.player.update();
 	}
 }
---
+++ js/Player.js
@@ -0,0 +1,36 @@
+export default class Player extends Phaser.Physics.Matter.Sprite {
+	constructor(data) {
+		let { scene, x, y, texture, frame } = data;
+		
+		super(scene.matter.world, x, y, texture, frame);
+		this.scene.add.existing(this);
+		
+	}
+
+	static preload(scene) {
+		scene.load.atlas('townsfolk', 'assets/images/townsfolk.png', 'assets/images/townsfolk_atlas.json');
+		scene.load.animation('townsfolk_anim', 'assets/images/townsfolk_anim.json');
+	}
+
+	update() {
+		this.anims.play('townsfolk_walk', true);
+		const speed = 2.5;
+		let playerVelocity = new Phaser.Math.Vector2();
+
+		if (this.inputKeys.left.isDown) {
+			playerVelocity.x = -1;
+		} else if (this.inputKeys.right.isDown) {
+			playerVelocity.x = 1;
+		}
+
+		if (this.inputKeys.up.isDown) {
+			playerVelocity.y = -1;
+		} else if (this.inputKeys.down.isDown) {
+			playerVelocity.y = 1;
+		}
+
+		playerVelocity.normalize();
+		playerVelocity.scale(speed);
+		this.setVelocity(playerVelocity.x, playerVelocity.y);
+	}
+}
